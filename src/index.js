2a587633daf3040dcf077e7c7870cd869cc4856b
Chris-Merced <148519061+Chris-Merced@users.noreply.github.com>
2024-10-22 12:05:15 -0500

    Create components folder and home.js to begin routing structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom';
+import WelcomeHome from './components/home';
 
-const App = () => <h1>hello world</h1>;
 const container = (document.getElementById('root'));
 const root = createRoot(container)
 
-root.render(<App />);
+root.render(<WelcomeHome />);
