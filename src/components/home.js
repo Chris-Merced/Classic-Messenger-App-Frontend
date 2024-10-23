711d02ff84778b44465f0f33501e1679ab31e2cd
Chris-Merced <148519061+Chris-Merced@users.noreply.github.com>
2024-10-23 12:06:44 -0500

    Add websocket functionality

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,59 @@
 import React from "react";
+import { useState, useEffect, useRef} from "react";
 
-function WelcomeHome() {
+const WebSocketComponent = () => {
+    const[message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+    const socketRef = useRef(null);
+    
+    useEffect(() => {
+        socketRef.current = new WebSocket("ws://localhost:3000")
+
+        socketRef.current.onopen = () => {
+            console.log("Connection Open");
+        };
+
+        socketRef.current.onmessage = (message) => {
+            console.log("message recieved");
+            setMessages((prevMessages) => [...prevMessages, message.data]);
+        };
+
+        socketRef.current.onclose = () => {
+            console.log("connection closed");
+        };
+
+        socketRef.current.onerror = (error) => {
+            console.error("WebSocket Error:", error);
+        };
+
+        return () => {
+            socketRef.current.close();
+        }
+    }, [])
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        console.log(message);
+        if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+            socketRef.current.send(message);
+            setMessage("");
+        }
+        else { console.log("console is not open") }
+    }
+
     return (
         <>
             <h1>Welcome Home</h1>
+            <ul>
+                {messages.map((message, index) => (
+                    <li key={index}>{message}</li>
+                ))}
+            </ul>
+            <form>
+                <input type="text" onChange={(e) => setMessage(e.target.value)} value={message}></input>
+                <button onClick={sendMessage}>Send Message</button>
+            </form>
         </>
     );
-};
-
+}
 export default WelcomeHome;
