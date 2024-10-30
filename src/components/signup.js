5b19713fa053888bf5fb59c9c0f1e32b6bb847cc
Chris-Merced <148519061+Chris-Merced@users.noreply.github.com>
2024-10-30 12:41:21 -0500

    Left Comment for next steps moving forward

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+//set Up Basic routing functionality
+//
+//Need to set up sign up functionality via a form submissiom
+//to interact with the backend
+//once the backend connection has been established we sanitize with error messages
 
 const signUpComponent = () => {
     return (
