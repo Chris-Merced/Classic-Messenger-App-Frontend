2d6186100a52c1aac3eb18d9f50c4f079ad26afa
Chris-Merced <148519061+Chris-Merced@users.noreply.github.com>
2024-11-14 13:11:09 -0600

    On loginHandler add "credentials" variable set to "include" to send and recieve cookies for storage

--- src/components/header.js
+++ src/components/header.js
@@ -15,7 +15,8 @@ const headerComponent = () => {
         const response = await fetch("http://localhost:3000/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data)
+            body: JSON.stringify(data),
+            credentials:'include'
         });
 
         const newData = await response.json();
