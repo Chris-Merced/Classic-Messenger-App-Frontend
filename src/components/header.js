1859b393c728c79de4ea372fbe68a2f5e26bf0e4
Chris-Merced <148519061+Chris-Merced@users.noreply.github.com>
2024-11-21 12:09:15 -0600

    Modify header to dynamically change based on user login status Modify password input field to hide input Change username and password states to revert to falsy after login attempt

--- src/components/header.js
+++ src/components/header.js
@@ -9,17 +9,22 @@ const headerComponent = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [dropDown, setDropDown] = useState(false);
-
+    const [user, setUser] = useState("");
+    
     const context = useContext(UserContext);
     const userData = context.user;
     
+    useEffect(() => {
+        setUser(userData)
+    }, [userData])
+
     const isDropDown = () => {
         if (dropDown === false) {
             setDropDown(true);
         } else { setDropDown(false) }
     }
 
-    const logout = async () => {
+    const logoutHandler = async () => {
         try {
             const response = await fetch('http://localhost:3000/logout', {
                 method: "DELETE",
@@ -27,11 +32,12 @@ const headerComponent = () => {
             })
 
             const data = await response.json();
+            setUser('');
         } catch (err) {
             console.log("Error with fetch: ", err);
         }
     }
-
+    
     const loginHandler = async (e) => {
         e.preventDefault();
         
@@ -39,7 +45,6 @@ const headerComponent = () => {
             username: username,
             password: password
         }
-        
         const response = await fetch("http://localhost:3000/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -47,22 +52,27 @@ const headerComponent = () => {
             credentials:'include'
         });
 
-        const newData = await response.json();
-        if (newData) {
-            console.log(newData.message);
+        const newUser = await response.json();
+        if (newUser) {
+            setUser(newUser);
+            console.log(newUser);
         } else {console.log("no data recieved")}
+
+        setPassword('');
+        setUsername('');
+
 
     }
 
     return (
         <div>
             <h1>Welcome Home</h1>
-            {userData ? (<><div> Hello {userData.username}</div>
+            {user ? (<><div> Hello {user.username}</div>
                 <div className="DropDown">
                     <button onClick={isDropDown}>Drop-Down</button>
                     {dropDown && <div className="Menu">
-                        <div>Hellow world</div>
-                        <button className="logout" onClick={logout}>Log Out</button>
+                        <div>User Menu: </div>
+                        <button className="logout" onClick={logoutHandler}>Log Out</button>
                     </div>}
                 </div>
             </>) : (
@@ -70,7 +80,7 @@ const headerComponent = () => {
                     <label htmlFor="username">Username: </label>
                     <input name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
                     <label htmlFor="password">Password: </label>
-                    <input name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                    <input name="password" id="password" type="password" onChange={(e) => setPassword(e.target.value)}></input>
                     <button type="submit">Log In</button>
                 </form>
             )}
