12e27ef85623054ab84c89160205287155ab6149
Chris-Merced <148519061+Chris-Merced@users.noreply.github.com>
2024-12-16 14:03:00 -0600

    Change signup url to be compatible with both back and front end using environment variabls

--- src/components/signup.js
+++ src/components/signup.js
@@ -53,7 +53,7 @@ const signUpComponent = () => {
         password: password,
       };
       try {
-        const response = await fetch('http://localhost:3000/signup', {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/signup`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(data),
