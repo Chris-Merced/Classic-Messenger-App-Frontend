a0559b17bea3433ef307496b0068298885d4f2d9
Chris-Merced <christopher.r.merced@gmail.com>
2025-03-21 14:04:36 -0500

    Add public folder to serve to heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('*', (req, res) => {
