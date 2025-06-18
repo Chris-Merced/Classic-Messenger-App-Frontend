18b3c38b2669d82845b3e1367a7472f0a8b2d606
Chris-Merced <christopher.r.merced@gmail.com>
2025-06-18 14:15:10 -0500

    Remove debug logs Fully implement checkFriendRequest function and display request status

--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -95,8 +95,7 @@ const UserProfile = () => {
         }
       );
       const data = await response.json();
-
-      console.log(data);
+      setRequestSent(data)
     };
 
     checkFriendRequest();
