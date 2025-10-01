2fbea127cfe44d6937880fd6e4bc81c150b2fb55
Chris-Merced <christopher.r.merced@gmail.com>
2025-10-01 14:12:49 -0500

    Add form with verfication to admin panel to create admins

--- src/components/admin.js
+++ src/components/admin.js
@@ -4,9 +4,19 @@ import { UserContext } from "../context/userContext";
 
 function AdminPanel() {
   const userContext = useContext(UserContext).user;
-  const [usernameError, setUsernameError] = useState("");
+
+  const [banUsernameError, setBanUsernameError] = useState("");
   const [daysError, setDaysError] = useState("");
   const [banSuccess, setBanSuccess] = useState(false);
+  const [banError, setBanError] = useState(false);
+
+  const [unbanUsernameError, setUnbanUsernameError] = useState("");
+  const [unbanSuccess, setUnbanSuccess] = useState(false);
+  const [unbanError, setUnbanError] = useState(false);
+
+  const [adminError, setAdminError] = useState(false);
+  const [adminUsernameError, setAdminUsernameError] = useState(false);
+  const [adminSuccess, setAdminSuccess] = useState(false);
 
   async function handleBan(e) {
     e.preventDefault();
@@ -17,7 +27,7 @@ function AdminPanel() {
 
     const username = e.target.username.value;
     const days = e.target.days.value;
-    let error = false
+    let error = false;
 
     if (days === "perm") {
       //perm ban
@@ -28,30 +38,109 @@ function AdminPanel() {
         setDaysError("");
       } else {
         setDaysError("Invalid Input: Enter integer or 'perm' ");
-        error=true;
+        error = true;
       }
     }
 
     if (!error) {
       const res = await fetch(
-        `${process.env.REACT_APP_BACKEND_URL}/admin/ban?username=${username}&days=${days.trim()}`,
+        `${
+          process.env.REACT_APP_BACKEND_URL
+        }/admin/ban?username=${username}&days=${days.trim()}`,
         {
           credentials: "include",
           method: "POST",
           headers: { "Content-Type": "application/json" },
         }
       );
+      const response = await res.json();
+
       if (!res.ok) {
+        console.log(response.error);
+        setBanError(true);
       } else {
-        const data = await res.json()
-        console.log(data)
+        const data = await res.json();
+        console.log(data);
         console.log("Successful ban");
+        setBanError(false);
         setBanSuccess(true);
         setTimeout(() => {
           setBanSuccess(false);
         }, 5000);
       }
-      
+    }
+  }
+
+  async function handleUnban(e) {
+    e.preventDefault();
+    const username = e.target.username.value;
+    if (typeof username !== "string" || username.length > 16) {
+      setUnbanUsernameError(true);
+      return;
+    }
+    const data = { username: username };
+    const res = await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/admin/unban`,
+      {
+        method: "PATCH",
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }
+    );
+
+    const response = await res.json();
+
+    if (res.ok) {
+      setUnbanError(false);
+      setUnbanSuccess(true);
+      setBanUsernameError(false);
+      setTimeout(() => {
+        setUnbanSuccess(false);
+      }, 5000);
+
+      console.log(response.message);
+    } else {
+      console.log(response.error);
+      setUnbanError(true);
+    }
+  }
+
+  async function handleMakeAdmin(e) {
+    e.preventDefault();
+
+    const username = e.target.username.value;
+    
+    if (typeof username !== "string" || username.length > 16) {
+      setAdminError(true);
+      return;
+    }
+
+    const data = { username: username };
+
+    const res = await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/admin/adminStatus`,
+      {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify(data),
+      }
+    );
+
+    const response = await res.json();
+
+    if (res.ok) {
+      setAdminError(false);
+      setAdminUsernameError(false);
+      setAdminSuccess(true);
+      setTimeout(() => {
+        setAdminSuccess(false);
+      }, 5000);
+      console.log(response.message);
+    } else {
+      console.log(response.error);
+      setAdminError(true);
     }
   }
 
@@ -62,10 +151,13 @@ function AdminPanel() {
           <header className="banHeader">Ban Form</header>
           <form className="banForm" onSubmit={handleBan}>
             <div className="banFormRow">
+              {banError && (
+                <div>Could not ban user, check console and try again</div>
+              )}
               <label htmlFor="username">Username: </label>
               <input className="usernameInput" id="username"></input>
             </div>
-            <div className="formError">{usernameError}</div>
+            <div className="formError">{banUsernameError}</div>
 
             <div className="banFormRow">
               <label htmlFor="banFormInput">Days: </label>
@@ -73,12 +165,46 @@ function AdminPanel() {
             </div>
             {daysError && <div className="formError">{daysError}</div>}
             <button className="banButton" type="submit">
-              Click me
+              Ban
             </button>
           </form>
           {banSuccess && (
             <div className="banSuccess">User Banned Successfully!</div>
           )}
+
+          <header className="banHeader">Unban Form</header>
+          {unbanError && (
+            <div>Could not unban user, check console and try again</div>
+          )}
+          <form className="banForm" onSubmit={handleUnban}>
+            <div className="banFormRow">
+              <label htmlFor="username">Username: </label>
+              <input className="usernameInput" id="username"></input>
+            </div>
+            {unbanUsernameError && <div>Invalid Username input</div>}
+            <button className="banButton" type="submit">
+              Unban
+            </button>
+          </form>
+          {unbanSuccess && (
+            <div className="banSuccess">User Unbanned Successfully!</div>
+          )}
+
+          <header className="banHeader">Make Admin Form</header>
+          {adminError && (
+            <div>Could not make Admin, check console and try again</div>
+          )}
+          <form className="banForm" onSubmit={handleMakeAdmin}>
+            <div className="banFormRow">
+              <label htmlFor="username">Username: </label>
+              <input className="usernameInput" id="username"></input>
+            </div>
+            {adminUsernameError && <div>Invalid Username input</div>}
+            <button className="banButton" type="submit">
+              Unban
+            </button>
+          </form>
+          {adminSuccess && <div className="banSuccess">User Made Admin!</div>}
         </>
       ) : (
         <div>You do not have permissions to view this page</div>
